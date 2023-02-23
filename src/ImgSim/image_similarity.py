a83e1b87fe73cc46cd41ae51462512953345d887
totogot <fergus.a.reid@gmail.com>
2023-02-23 16:58:18 +0000

    adding comments and function guidance

--- src/ImgSim/image_similarity.py
+++ src/ImgSim/image_similarity.py
@@ -1,28 +1,56 @@
-#import numpy as np
-#import pandas as pd
 import torch
-from PIL import Image
-import matplotlib.pyplot as plt
-import torch.nn as nn
-#import itertools
 import os
 
+import matplotlib.pyplot as plt
+import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as transforms
 
+from PIL import Image
 
-class Img2Vec():
 
-    def __init__(self, model_name, weights='DEFAULT'):
+class Img2Vec:
+    """
+    Class for embedding dataset of image files into vectors using Pytorch
+    standard neural networks.
 
-        # dictionary defining the supported embeddings
+    Parameters:
+    -----------
+    model_name: str object specifying neural network architecture to utilise.
+        Must align to the naming convention specified on Pytorch documentation:
+        https://pytorch.org/vision/main/models.html#classification
+        For supported model architectures see self.embed_dict below
+    weights: str object specifying the pretrained weights to load into model.
+        Only weights supported by Pytorch torchvision library can be accessed.
+        Current functionality reverts to DEFAULT weights if no specified.
+
+    See also:
+    -----------
+    Img2Vec.embed_dataset(): embed passed images as feature vectors
+    Img2Vec.save_dataset(): save embedded dataset to file for future loading
+    Img2Vec.load_dataset(): load previously embedded dataset of feature vectors
+    Img2Vec.similar_image(): pass target image and return most similar image(s)
+
+    Example:
+    -----------
+
+    ImgSim = imgsim.Img2Vec('resnet50', weights='DEFAULT')
+    ImgSim.embed_dataset('[EXAMPLE PATH TO DIRECTORY OF IMAGES]')
+
+    ImgSim.save_dataset('[OUTPUT PATH FOR SAVING EMBEDDEDINGS]')
+
+    ImgSim.similar_images('[EXAMPLE PATH TO TARGET IMAGE]')
+    """
+
+    def __init__(self, model_name, weights="DEFAULT"):
+        # dictionary defining the supported NN architectures
         self.embed_dict = {
             "resnet50": self.obtain_children,
-            "vgg19":  self.obtain_classifier,
-            "efficientnet_b0": self.obtain_classifier
-            }
-        
-        # assign class parameters
+            "vgg19": self.obtain_classifier,
+            "efficientnet_b0": self.obtain_classifier,
+        }
+
+        # assign class attributes
         self.architecture = self.validate_model(model_name)
         self.weights = weights
         self.transform = self.assign_transform(weights)
@@ -31,94 +59,79 @@ class Img2Vec():
         self.embed = self.assign_layer()
         self.dataset = {}
 
-    
     def validate_model(self, model_name):
         if model_name not in self.embed_dict.keys():
-            raise ValueError(f'The model {model_name} is not supported')
+            raise ValueError(f"The model {model_name} is not supported")
         else:
             return model_name
 
-    
     def assign_transform(self, weights):
-       
         weights_dict = {
             "resnet50": models.ResNet50_Weights,
             "vgg19": models.VGG19_Weights,
-            "efficientnet_b0":models.EfficientNet_B0_Weights
+            "efficientnet_b0": models.EfficientNet_B0_Weights,
         }
 
+        # try load preprocess from torchvision else assign default
         try:
             w = weights_dict[self.architecture]
             weights = getattr(w, weights)
-            preprocess =  weights.transforms()
-        except:
-            preprocess = transforms.Compose([
-                transforms.Resize(224),
-                transforms.ToTensor(),
-                transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-                )
-                ])
+            preprocess = weights.transforms()
+        except Exception:
+            preprocess = transforms.Compose(
+                [
+                    transforms.Resize(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    ),
+                ]
+            )
 
         return preprocess
 
-    
     def set_device(self):
         if torch.cuda.is_available():
             device = "cuda:0"
         else:
             device = "cpu"
-        
+
         return device
-    
-    
+
     def initiate_model(self):
-        
-        m = getattr(models, self.architecture) # equ to assigning m as models.resnet50() 
-        model = m(weights = self.weights) # equ to models.resnet50(weights = ...)
+        m = getattr(
+            models, self.architecture
+        )  # equ to assigning m as models.resnet50()
+        model = m(weights=self.weights)  # equ to models.resnet50(weights=...)
         model.to(self.device)
-        
+
         return model.eval()
-    
 
     def assign_layer(self):
         model_embed = self.embed_dict[self.architecture]()
 
         return model_embed
-    
 
     def obtain_children(self):
         model_embed = nn.Sequential(*list(self.model.children())[:-1])
-        
-        return model_embed
 
+        return model_embed
 
     def obtain_classifier(self):
         self.model.classifier = self.model.classifier[:-1]
-        
+
         return self.model
-      
 
-    def directory_to_list(self, source):
+    def directory_to_list(self, dir):
+        ext = (".png", ".jpg", ".jpeg")
 
-        types = (
-            '.png',
-            '.jpg',
-            '.jpeg'
-        )
-        
-        dir_list = os.listdir(source)
-        source_list = [
-            os.path.join(source, f) for f in dir_list 
-            if os.path.splitext(f)[1] in types
-            ]
+        d = os.listdir(dir)
+        source_list = [os.path.join(dir, f) for f in d if os.path.splitext(f)[1] in ext]
 
         return source_list
-        
-    
-    def validate_source(self, source):
 
+    def validate_source(self, source):
+        # convert source format into standard list of file paths
         if isinstance(source, list):
             source_list = [f for f in source if os.path.isfile(f)]
         elif os.path.isdir(source):
@@ -127,100 +140,121 @@ class Img2Vec():
             source_list = [source]
         else:
             raise ValueError('"source" expected as file, list or directory.')
-        
+
         return source_list
 
-    
     def embed_image(self, img):
-        
+        # load and preprocess image
         img = Image.open(img)
         img_trans = self.transform(img)
 
+        # store computational graph on GPU is available
         if self.device == "cuda:0":
             img_trans = img_trans.cuda()
-        
+
         img_trans = img_trans.unsqueeze(0)
 
         return self.embed(img_trans)
 
-    
     def embed_dataset(self, source):
-        
-        self.files = self.validate_source(source)    
+        # convert source to appropriate format
+        self.files = self.validate_source(source)
 
         for file in self.files:
             vector = self.embed_image(file)
             self.dataset[str(file)] = vector
-        
+
         return
-            
-    
+
     def similar_images(self, target_file, n=None):
+        """
+        Function for comparing target image to embedded image dataset
+
+        Parameters:
+        -----------
+        target_file: str specifying the path of target image to compare
+            with the saved feature embedding dataset
+        n: int specifying the top n most similar images to return
+        """
 
         target_vector = self.embed_image(target_file)
+
+        # initiate computation of consine similarity
         cosine = nn.CosineSimilarity(dim=1)
 
+        # iteratively store similarity of stored images to target image
         sim_dict = {}
         for k, v in self.dataset.items():
             sim = cosine(v, target_vector)[0].item()
             sim_dict[k] = sim
-        
-        sim_dict = {k: v for k, v in sorted(sim_dict.items(), 
-                                            key=lambda item: item[1], 
-                                            reverse=True)}
 
+        # sort based on decreasing similarity
+        items = sim_dict.items()
+        sim_dict = {
+            k: v for k, v in sorted(items, key=lambda item: item[1], reverse=True)
+        }
+
+        # cut to defined top n similar images
         if n is not None:
-            sim_dict = dict(list(sim_dict.items())[:n])
+            sim_dict = dict(list(sim_dict.items())[: int(n)])
 
         self.output_images(sim_dict, target_file)
 
         return sim_dict
-    
 
     def output_images(self, similar, target):
-
-        self.display_img(target, 'original')
+        self.display_img(target, "original")
 
         for k, v in similar.items():
-            self.display_img(k, 'similarity:' + str(v))
+            self.display_img(k, "similarity:" + str(v))
 
         return
-    
 
     def display_img(self, path, title):
-
         plt.imshow(Image.open(path))
-        plt.axis('off')
+        plt.axis("off")
         plt.title(title)
         plt.show()
 
         return
 
-
     def save_dataset(self, path):
-        
-        data = {
-            "model": self.architecture,
-            "embeddings": self.dataset
-            }
-        
-        torch.save(data, os.path.join(path,'tensors.pt')) #need to update functionality for naming convention
+        """
+        Function to save a previously embedded image dataset to file
+
+        Parameters:
+        -----------
+        path: str specifying the output folder to save the tensors to
+        """
+
+        # convert embeddings to dictionary
+        data = {"model": self.architecture, "embeddings": self.dataset}
+
+        torch.save(
+            data, os.path.join(path, "tensors.pt")
+        )  # need to update functionality for naming convention
 
         return
-    
 
     def load_dataset(self, source):
+        """
+        Function to save a previously embedded image dataset to file
+
+        Parameters:
+        -----------
+        source: str specifying tensor.pt file to load previous embeddings
+        """
 
         data = torch.load(source)
 
+        # assess that embedding nn matches currently initiated nn
         if data["model"] == self.architecture:
             self.dataset = data["embeddings"]
         else:
             raise AttributeError(
                 f'NN architecture "{self.architecture}" does not match the '
-                +f'"{data["model"]}" model used to generate saved embeddings. '
-                +'Re-initiate Img2Vec with correct architecture and reload.'
-                )
-        
+                + f'"{data["model"]}" model used to generate saved embeddings.'
+                + " Re-initiate Img2Vec with correct architecture and reload."
+            )
+
         return
-        
